4c5f0393c470a8b62d0a26cb4906a3288454af0b
mvstermind <zablockidawid1@gmail.com>
2024-09-21 23:51:04 +0200

    TFFFF THIS BUG IS CURSED

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/mvstermind/tool/cmd"
 )
@@ -12,6 +14,28 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(url)
 
+	dir, err := os.ReadFile("./lists/dir-list.txt")
+	if err != nil {
+		log.Println("couldn't read file contents!")
+	}
+
+	_ = formUrls(url, dir)
+
+}
+
+func formUrls(baseUrl string, dirNames []byte) []string {
+	var stringified []string
+	var lc int
+	for i := 0; i < len(dirNames); i++ {
+		if dirNames[i] == '\n' {
+			stringified = append(stringified, string(dirNames[i-lc:i]))
+			lc = 0
+		} else {
+			lc++
+		}
+	}
+	fmt.Println(stringified)
+
+	return nil
 }
